16d738f6d73b11e5b2e88473b1572630aefffdaf
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-31 18:32:22 -0500

    commit - DivisionDAO / .

---
+++ src/dao/DivisionDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import db.ConexionOracle;
+import model.Division;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+
+public class DivisionDAO {
+
+    public void insertar(Division d) throws Exception {
+        String sql = "{ call SP_INSERTAR_DIVISION(?,?,?) }";
+        try (Connection cn = ConexionOracle.getConexion();
+             CallableStatement cs = cn.prepareCall(sql)) {
+
+            cs.setBigDecimal(1, d.getA());
+            cs.setBigDecimal(2, d.getB());
+            cs.setBigDecimal(3, d.getResultado());
+            cs.execute();
+        }
+    }
+}   
